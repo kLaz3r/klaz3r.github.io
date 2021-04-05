4fc8a95a95f11ce69ca28d4185c420782da798bc
BuildTools <unconfigured@null.spigotmc.org>
2021-04-05 22:33:07 +0300

    format

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-
 function love() {
- var result = document.getElementById("result")
- var input = document.getElementById("nume").value;
- if (input === "Dana") {
-  result.innerHTML = "Te iubeste enorm de mult";
- } else if (input === "dana") {
-   result.innerHTML = "Te iubeste enorm de mult";
- } else if (input === "Dana Obreja") {
-   result.innerHTML = "Te iubeste enorm de mult";
- } else if (input === "dana obreja") {
-   result.innerHTML = "Te iubeste enorm de mult";
- } else if (input === "gabi") {
-   result.innerHTML = "Te pup la portofel";
-   } else {
-  result.innerHTML =
-   "Nu te iubeste te drq de coarda";
- }
- console.log(input);
+  var result = document.getElementById("result");
+  var input = document.getElementById("nume").value;
+  if (input === "Dana") {
+    result.innerHTML = "Te iubeste enorm de mult";
+  } else if (input === "dana") {
+    result.innerHTML = "Te iubeste enorm de mult";
+  } else if (input === "Dana Obreja") {
+    result.innerHTML = "Te iubeste enorm de mult";
+  } else if (input === "dana obreja") {
+    result.innerHTML = "Te iubeste enorm de mult";
+  } else if (input === "gabi") {
+    result.innerHTML = "Te pup la portofel";
+  } else {
+    result.innerHTML = "Nu te iubeste te drq de coarda";
+  }
+  console.log(input);
 }
